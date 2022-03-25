744faa2a1ee15c52bb7876e17d0565355259c242
Raj <rajkhulna@outlook.com>
2022-03-26 00:52:55 +0600

    Initial Commit

---
+++ Unit Conversion_TIME/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unit Conversion_TIME
+//
+//  Created by Khalid Khan on 26/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Unit Conversion_TIME/Unit_Conversion_TIMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_Conversion_TIMEApp.swift
+//  Unit Conversion_TIME
+//
+//  Created by Khalid Khan on 26/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Unit_Conversion_TIMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
